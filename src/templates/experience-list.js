9a06b52b16284400ebc23f453b86e04efc4402b5
Tyler <ouell117@uwindsor.ca>
2020-01-29 16:59:28 -0500

    swap to experience

---
+++ src/templates/experience-list.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import BlogItems from "../components/items-experience";
+import SectionTitle from "../components/sectiontitle";
+import Pagination from "../components/pagination";
+import SEO from "../components/seo";
+
+class BlogList extends React.Component {
+    render() {
+        const query = this.props.datas;
+        if (query.allMarkdownRemark.edges.length > 0) {
+            return (
+                <section id="experience" className="container">
+                    <div className="section-title">
+                        <SectionTitle title="EXPERIENCE" />
+                    </div>
+                    <BlogItems data={query} />
+                    <Pagination
+                        pathContext={this.props.pathContext}
+                        type="experience"
+                    />
+                </section>
+            );
+        } else {
+            return <React.Fragment></React.Fragment>;
+        }
+    }
+}
+
+export default function({ data, pathContext }) {
+    return (
+        <Layout>
+            <SEO lang="en" title="Experience" />
+            <BlogList datas={data} pathContext={pathContext} />
+        </Layout>
+    );
+}
+
+export const query = graphql`
+    query experienceListPage($skip: Int!, $limit: Int!) {
+        allMarkdownRemark(
+            filter: { fileAbsolutePath: { regex: "/experience/" } }
+            sort: { fields: [frontmatter___date], order: DESC }
+            limit: $limit
+            skip: $skip
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        description
+                        date
+                        image {
+                            publicURL
+                            childImageSharp {
+                                fluid(maxWidth: 1920) {
+                                    srcSet
+                                    ...GatsbyImageSharpFluid
+                                }
+                                id
+                            }
+                        }
+                    }
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+    }
+`;
---
+++ src/templates/experience.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import LatestPosts from "../components/experienceposts-latest";
+import SEO from "../components/seo";
+import Date from "../components/date";
+import "../style/experience-singlepage.less";
+
+export default function({ data }) {
+    return (
+        <Layout>
+            <SEO
+                lang="en"
+                title={data.markdownRemark.frontmatter.title}
+                description={data.markdownRemark.frontmatter.description}
+                image={data.markdownRemark.frontmatter.image.publicURL}
+            />
+            <div className="container">
+                <article className="experience-post">
+                    <div className="head text-primary">
+                        <h1>{data.markdownRemark.frontmatter.title}</h1>
+                        <p className="post-date">
+                            <Date data={data.markdownRemark.frontmatter.date} />
+                        </p>
+                    </div>
+                    <div className="content row flex">
+                        <div
+                            className="col s12 m11 l10"
+                            dangerouslySetInnerHTML={{
+                                __html: data.markdownRemark.html
+                            }}
+                        ></div>
+                    </div>
+                </article>
+            </div>
+        </Layout>
+    );
+}
+
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            html
+            id
+            frontmatter {
+                title
+                date
+                description
+                image {
+                    publicURL
+                    childImageSharp {
+                        fluid(maxWidth: 1000) {
+                            srcSet
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
