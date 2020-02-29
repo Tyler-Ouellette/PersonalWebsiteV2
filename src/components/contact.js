88af229a509dbc4c4d21b908771f31e7c941668b
Tyler <ouell117@uwindsor.ca>
2020-02-29 14:32:08 -0500

    netlify form and commented a bunch out

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,17 +1,17 @@
-import React from "react";
-import SectionTitle from "./sectiontitle";
-import { StaticQuery, graphql } from "gatsby";
-import { PaperPlane, Mapmarker, Mobile, Envelope, Loading } from "./icons";
-import SocialLinks from "./sociallinks";
-import "../style/contact.less";
-import axios from "axios";
+import React from 'react';
+import SectionTitle from './sectiontitle';
+import { StaticQuery, graphql } from 'gatsby';
+import { PaperPlane, Mapmarker, Mobile, Envelope, Loading } from './icons';
+import SocialLinks from './sociallinks';
+import '../style/contact.less';
+import axios from 'axios';
 
 class Contact extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            submitDisabled: false
+            submitDisabled: false,
         };
 
         this.textAreaInput = this.textAreaInput.bind(this);
@@ -19,71 +19,69 @@ class Contact extends React.Component {
 
         this.showContactForm = true;
 
-        if (this.props.contact.api_url === "") {
+        if (this.props.contact.api_url === '') {
             this.showContactForm = false;
         }
     }
 
     textAreaInput(event) {
-        event.target.style.height = "auto";
-        event.target.style.height = event.target.scrollHeight + "px";
+        event.target.style.height = 'auto';
+        event.target.style.height = event.target.scrollHeight + 'px';
     }
 
     handleSubmit(event) {
         event.preventDefault();
         if (!this.state.submitDisabled) {
             this.setState({
-                submitDisabled: true
+                submitDisabled: true,
             });
 
             let name = encodeURI(this.dataName.value),
                 email = encodeURI(this.dataEmail.value),
-                message = encodeURI(this.dataMessage.value),
-                body = `name=${name}&email=${email}&message=${message}`;
+                message = encodeURI(this.dataMessage.value);
+            console.log(name, email, message);
+            // body = `name=${name}&email=${email}&message=${message}`;
 
-            const html = convertToHtml(message);
+            // const html = convertToHtml(message);
 
-            let json = {
-                From: { email },
-                To: "ouellette.tyler@hotmail.com",
-                Subject: "Resume Site Contact",
-                HtmlBody: { message },
-                TextBody: { message },
-                Headers: [
-                    {
-                        Name: "X-Postmark-Server-Token",
-                        Value: `${process.env.TOKEN}`
-                    }
-                ]
-            };
+            // const instance = axios.create({
+            //     url: 'https://api.postmarkapp.com',
+            //     method: 'POST',
+            //     headers: {
+            //         'X-Postmark-Server-Token': '49c4787c-b74d-4518-9f9d-94874cea767e',
+            //         'Access-Control-Allow-Origin': '*',
+            //         Vary: 'Origin',
+            //         'Content-Type': 'application/json',
+            //         Accept: 'application/json',
+            //     },
+            //     dataType: 'application/json',
+            //     data: {
+            //         From: { name, email },
+            //         To: 'ouellette.tyler@hotmail.com',
+            //         Subject: 'Contact Form Resume Site',
+            //         HtmlBody: { message },
+            //         TextBody: { message },
+            //         ReplyTo: { email },
+            //         Headers: [
+            //             { Name: 'X-Postmark-Server-Token', Value: '49c4787c-b74d-4518-9f9d-94874cea767e' },
+            //             { Name: 'Access-Control-Allow-Origin', Value: '*' },
+            //             { Name: 'Vary', Value: 'Origin' },
+            //         ],
+            //         TrackOpens: true,
+            //         TrackLinks: 'HtmlOnly',
+            //     },
+            // });
 
-            // axios
-            //     .post("http://https://api.postmarkapp.com/email", {
-            //         body: json
-            //     })
+            console.log(instance.defaults);
 
-            const instance = axios
-                .create({
-                    url: "https://api.postmarkapp.com/email",
-                    method: "post",
-                    dataType: "json",
-                    data: {
-                        From: { email },
-                        Subject: "Resume Website Contact",
-                        HtmlBody: { message },
-                        TextBody: { message },
-                        Headers: [
-                            {
-                                Name: "X-Postmark-Server-Token",
-                                Value: `${process.env.TOKEN}`
-                            }
-                        ]
-                    },
-                    To: "ouellette.tyler@hotmail.com"
-                })
-                // fetch("http://localhost/local/test.json", {
-                //     method: "post",
-                //     body: body
+            axios
+                .post('https://api.postmarkapp.com/email', { instance })
+                // .post('https://api.postmarkapp.com/email', instance.defaults, {
+                //     'X-Postmark-Server-Token': '49c4787c-b74d-4518-9f9d-94874cea767e',
+                //     'Access-Control-Allow-Origin': '*',
+                //     Vary: 'Origin',
+                //     'Content-Type': 'application/json',
+                //     Accept: 'application/json',
                 // })
                 .then(function(res) {
                     return res.json();
@@ -91,31 +89,29 @@ class Contact extends React.Component {
                 .then(
                     result => {
                         this.setState({
-                            submitDisabled: false
+                            submitDisabled: false,
                         });
                         this.resMessage.style.opacity = 1;
-                        if (result.response === "error") {
-                            this.resMessage.innerHTML =
-                                "There was an error in sending the message";
-                            this.resMessage.classList.add("color-error");
+                        if (result.response === 'error') {
+                            this.resMessage.innerHTML = 'There was an error in sending the message';
+                            this.resMessage.classList.add('color-error');
                         } else {
-                            this.resMessage.innerHTML =
-                                "Message sent succesfully";
-                            this.resMessage.classList.remove("color-error");
+                            this.resMessage.innerHTML = 'Message sent succesfully';
+                            this.resMessage.classList.remove('color-error');
                         }
-                        this.dataName.value = "";
-                        this.dataEmail.value = "";
-                        this.dataMessage.value = "";
+                        this.dataName.value = '';
+                        this.dataEmail.value = '';
+                        this.dataMessage.value = '';
                         let _this = this;
                         setTimeout(function() {
                             _this.resMessage.style.opacity = 0;
                         }, 5000);
                     },
                     error => {
-                        this.resMessage.innerHTML = "Message sent succesfully";
-                        this.resMessage.classList.remove("color-error");
+                        this.resMessage.innerHTML = 'Message sent succesfully';
+                        this.resMessage.classList.remove('color-error');
                         this.setState({
-                            submitDisabled: false
+                            submitDisabled: false,
                         });
                         let _this = this;
                         setTimeout(function() {
@@ -128,21 +124,15 @@ class Contact extends React.Component {
 
     componentDidMount() {
         if (this.showContactForm) {
-            let color = window
-                .getComputedStyle(this.btn, null)
-                .getPropertyValue("color");
-            this.btn.querySelector("path").setAttribute("fill", color);
+            let color = window.getComputedStyle(this.btn, null).getPropertyValue('color');
+            this.btn.querySelector('path').setAttribute('fill', color);
         }
 
-        let li = this.contactArea.querySelectorAll(".item");
+        let li = this.contactArea.querySelectorAll('.item');
 
         li.forEach(function(e, i) {
-            let p = e.querySelector("path");
-            if (p)
-                p.setAttribute(
-                    "fill",
-                    window.getComputedStyle(e, null).getPropertyValue("color")
-                );
+            let p = e.querySelector('path');
+            if (p) p.setAttribute('fill', window.getComputedStyle(e, null).getPropertyValue('color'));
         });
     }
 
@@ -152,18 +142,13 @@ class Contact extends React.Component {
                 <div className="section-title">
                     <SectionTitle title="CONTACT" />
                 </div>
-                <div
-                    className={"row" + (this.showContactForm ? "" : " no-form")}
-                    ref={c => (this.contactArea = c)}
-                >
+                <div className={'row' + (this.showContactForm ? '' : ' no-form')} ref={c => (this.contactArea = c)}>
                     {this.showContactForm && (
                         <div className="col s12 m6">
-                            <form>
+                            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
                                 <div className="field">
                                     <label>
-                                        <span className="label text-tertiary">
-                                            Name
-                                        </span>
+                                        <span className="label text-tertiary">Name</span>
                                         <div className="input-border">
                                             <input
                                                 type="text"
@@ -178,9 +163,7 @@ class Contact extends React.Component {
                                 </div>
                                 <div className="field">
                                     <label>
-                                        <span className="label text-tertiary">
-                                            Email
-                                        </span>
+                                        <span className="label text-tertiary">Email</span>
                                         <div className="input-border">
                                             <input
                                                 type="email"
@@ -195,15 +178,11 @@ class Contact extends React.Component {
                                 </div>
                                 <div className="field">
                                     <label>
-                                        <span className="label text-tertiary">
-                                            Message
-                                        </span>
+                                        <span className="label text-tertiary">Message</span>
                                         <div className="input-border">
                                             <textarea
-                                                style={{ overflowY: "hidden" }}
-                                                ref={c =>
-                                                    (this.dataMessage = c)
-                                                }
+                                                style={{ overflowY: 'hidden' }}
+                                                ref={c => (this.dataMessage = c)}
                                                 className="field-box"
                                                 onChange={this.textAreaInput}
                                                 name="message"
@@ -216,62 +195,37 @@ class Contact extends React.Component {
                                 <div className="field">
                                     <label className="ib">
                                         <button
-                                            className={
-                                                "btn" +
-                                                (this.state.submitDisabled
-                                                    ? " disabled"
-                                                    : "")
-                                            }
+                                            className={'btn' + (this.state.submitDisabled ? ' disabled' : '')}
                                             onClick={this.handleSubmit}
                                             id="submit"
-                                            ref={c => (this.btn = c)}
-                                        >
-                                            SEND{" "}
+                                            ref={c => (this.btn = c)}>
+                                            SEND{' '}
                                             <span
                                                 className="icon paper-plane"
                                                 style={{
-                                                    display: this.state
-                                                        .submitDisabled
-                                                        ? "none"
-                                                        : "inline-block"
-                                                }}
-                                            >
+                                                    display: this.state.submitDisabled ? 'none' : 'inline-block',
+                                                }}>
                                                 <PaperPlane />
                                             </span>
                                             <span
                                                 className="icon loading"
                                                 style={{
-                                                    display: !this.state
-                                                        .submitDisabled
-                                                        ? "none"
-                                                        : "inline-block"
-                                                }}
-                                            >
+                                                    display: !this.state.submitDisabled ? 'none' : 'inline-block',
+                                                }}>
                                                 <Loading />
                                             </span>
                                         </button>
                                     </label>
                                     <label>
-                                        <p
-                                            className="res-message"
-                                            ref={c => (this.resMessage = c)}
-                                        ></p>
+                                        <p className="res-message" ref={c => (this.resMessage = c)}></p>
                                     </label>
                                 </div>
                             </form>
                         </div>
                     )}
-                    <div
-                        className={
-                            this.showContactForm
-                                ? "col s12 m6 details"
-                                : "col s12 details"
-                        }
-                    >
+                    <div className={this.showContactForm ? 'col s12 m6 details' : 'col s12 details'}>
                         {this.props.contact.description && (
-                            <p className="text-tertiary">
-                                {this.props.contact.description}
-                            </p>
+                            <p className="text-tertiary">{this.props.contact.description}</p>
                         )}
                         <ul>
                             {this.props.contact.mail && (
@@ -279,13 +233,7 @@ class Contact extends React.Component {
                                     <span className="icon">
                                         <Envelope />
                                     </span>
-                                    <a
-                                        href={
-                                            "mailto:" + this.props.contact.mail
-                                        }
-                                    >
-                                        {this.props.contact.mail}
-                                    </a>
+                                    <a href={'mailto:' + this.props.contact.mail}>{this.props.contact.mail}</a>
                                 </li>
                             )}
                             {this.props.contact.phone && (
@@ -293,16 +241,11 @@ class Contact extends React.Component {
                                     <span className="icon">
                                         <Mobile />
                                     </span>
-                                    <a href={"tel:" + this.props.contact.phone}>
-                                        {this.props.contact.phone}
-                                    </a>
+                                    <a href={'tel:' + this.props.contact.phone}>{this.props.contact.phone}</a>
                                 </li>
                             )}
                             {this.props.contact.address && (
-                                <li
-                                    className="text-tertiary item"
-                                    style={{ whiteSpace: "pre" }}
-                                >
+                                <li className="text-tertiary item" style={{ whiteSpace: 'pre' }}>
                                     <span className="icon">
                                         <Mapmarker />
                                     </span>
