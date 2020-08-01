4c373662f8a76b80f509729ec3cf1e09754b5ad3
Tyler Ouellette <ouell117@uwindsor.ca>
2020-08-01 18:58:32 -0400

    add flexbox to experience items

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,8 @@
-import React from "react";
-import SectionTitle from "./sectiontitle";
-import { StaticQuery, graphql } from "gatsby";
-import "../style/contact.less";
-import Img from "gatsby-image";
+import React from 'react';
+import SectionTitle from './sectiontitle';
+import { StaticQuery, graphql } from 'gatsby';
+import '../style/contact.less';
+import Img from 'gatsby-image';
 
 class About extends React.Component {
     render() {
@@ -16,12 +16,9 @@ class About extends React.Component {
                                 <div className="center">
                                     <div className="img">
                                         <Img
-                                            fluid={
-                                                this.props.file.childImageSharp
-                                                    .fluid
-                                            }
+                                            fluid={this.props.file.childImageSharp.fluid}
                                             style={{
-                                                "border-radius": "50%"
+                                                'border-radius': '50%',
                                             }}
                                         />
                                     </div>
@@ -30,9 +27,8 @@ class About extends React.Component {
                                 <div
                                     className="col s12 m11 l10"
                                     dangerouslySetInnerHTML={{
-                                        __html: this.props.about.html
-                                    }}
-                                ></div>
+                                        __html: this.props.about.html,
+                                    }}></div>
                             </div>
                         </article>
                     </div>
@@ -76,8 +72,6 @@ export default () => (
                 }
             }
         `}
-        render={data => (
-            <About about={data.site.siteMetadata.about} file={data.file} />
-        )}
+        render={data => <About about={data.site.siteMetadata.about} file={data.file} />}
     />
 );
