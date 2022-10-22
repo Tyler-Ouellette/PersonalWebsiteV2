3e8b435a7ba1ba416e0a020b66a7effbdaec330f
tyler-ouellette <ouellette.tyler@hotmail.com>
2022-10-22 02:20:08 -0400

    package lock updates, include new portfolio items

--- src/components/list-portfolio.js
+++ src/components/list-portfolio.js
@@ -2,38 +2,36 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import PortfolioItems from "./items-portfolio";
 
-export default function() {
+export default function () {
     const query = useStaticQuery(graphql`
-        query portfolioList {
-            allMarkdownRemark(
-                filter: { fileAbsolutePath: { regex: "/portfolio/" } }
-                limit: 6
-                sort: { fields: [frontmatter___date], order: DESC }
-            ) {
-                edges {
-                    node {
-                        id
-                        frontmatter {
-                            title
-                            description
-                            image {
-                                publicURL
-                                childImageSharp {
-                                    fluid(maxWidth: 1920) {
-                                        srcSet
-                                        ...GatsbyImageSharpFluid
-                                    }
-                                    id
-                                }
-                            }
-                        }
-                        fields {
-                            slug
-                        }
+    query portfolioList {
+        allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/portfolio/"}}, limit: 6, sort: {fields: [frontmatter___date], order: DESC}) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                description
+                image {
+                  publicURL
+                  childImageSharp {
+                    fluid(maxWidth: 1920) {
+                      srcSet
+                      ...GatsbyImageSharpFluid
+                      base64
+                      originalImg
                     }
+                    id
+                  }
                 }
+              }
+              fields {
+                slug
+              }
             }
+          }
         }
+      }      
     `);
 
     return (
